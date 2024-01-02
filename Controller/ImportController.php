424279914bef11c5b10fe6a6e462383a7fb1b502
Kevin Papst <kpapst@gmx.net>
2024-01-02 18:50:15 +0100

    import from correct namespace

--- Controller/ImportController.php
+++ Controller/ImportController.php
@@ -26,7 +26,7 @@ use KimaiPlugin\ImportBundle\Model\TimesheetImportModel;
 use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route(path: '/importer')]
